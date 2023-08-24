7a3f66510d0ff6c98a27762a0ecf4281b9df1c6d
Ubuntu <ubuntu@ip-172-31-41-119.ec2.internal>
2023-08-24 06:05:16 +0000

    Initial Commit

---
+++ deploy_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Application Deploying..."
+# Add your deployment commands here
+# For example:
+sudo apt update
+# npm run build
+# pm2 restart my-app
+echo "Deployment completed."
